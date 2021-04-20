1f4a1a31d46d59979e515b5a557ddc5fbe7427b3
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-04-20 21:55:08 +0700

    added birthday reminder project

---
+++ Birthday Reminder/README.md
@@ -0,0 +1,5 @@
+Project from JohnSmilga.com
+
+## Idea
+
+[https://uidesigndaily.com/](https://uidesigndaily.com/posts/sketch-birthdays-list-card-widget-day-1042)
---
+++ Birthday Reminder/src/data.js
@@ -0,0 +1,37 @@
+export default [
+  {
+    id: 1,
+    name: `Anthony Edward "Tony" Stark`,
+    age: 40,
+    image:
+      "https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/robert-downey-jr-avengers-endgame-iron-man-1561980791.jpg?crop=0.529xw:1.00xh;0.0978xw,0&resize=480:*",
+  },
+  {
+    id: 2,
+    name: `Steven Grant "Steve" Rogers`,
+    age: 103,
+    image:
+      "https://i.pinimg.com/originals/d5/b4/1e/d5b41e6415089fa3f4735fe001c24417.jpg",
+  },
+  {
+    id: 3,
+    name: `Natalia Alianovna "Natasha" Romanoff`,
+    age: 37,
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1-LwpIHX8KjDIGhZ1wXaXhxWzxottDqSwww&usqp=CAU",
+  },
+  {
+    id: 4,
+    name: "Thor Odinson",
+    age: 1505,
+    image:
+      "https://cdn0-production-images-kly.akamaized.net/iXvjN3anmyxe0hzlPpX5BKEicVw=/197x0:712x515/640x640/filters:quality(75):strip_icc():format(jpeg)/kly-media-production/medias/1756933/original/039763400_1509458432-Thor_Ragnarok.jpg",
+  },
+  {
+    id: 5,
+    name: "Robert Bruce Banner",
+    age: 51,
+    image:
+      "https://pbs.twimg.com/profile_images/638103226382962693/piHD_6s1_400x400.jpg",
+  },
+];
