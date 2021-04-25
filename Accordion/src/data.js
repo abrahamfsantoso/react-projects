1d14f2efc2bfb9f40929a383eea17c7b13ccedcb
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-04-25 16:28:14 +0700

    added accordion

---
+++ Accordion/src/data.js
@@ -0,0 +1,33 @@
+const questions = [
+  {
+    id: 1,
+    title: "Do I have to allow the use of cookies?",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque iusto sequi qui consequatur harum facilis explicabo sunt placeat debitis ipsa omnis eos possimus, animi nulla expedita in nisi minus provident? Optio rem vitae beatae expedita, voluptatum praesentium accusamus voluptates architecto quaerat a consequuntur provident, corrupti dolorem illo dignissimos veniam veritatis!",
+  },
+  {
+    id: 2,
+    title: "How do I change my My Page password?",
+    info:
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Dignissimos fuga dicta nulla maxime quos quaerat rem, facere distinctio atque perspiciatis veritatis corporis ipsa totam ullam voluptate fugiat non, illum sunt?",
+  },
+  {
+    id: 3,
+    title: "What is BankID?",
+    info:
+      "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nisi qui accusamus laborum in voluptatum, aliquam culpa et id alias, nobis non minima, explicabo deleniti ipsam.",
+  },
+  {
+    id: 4,
+    title: "Whose birth number can I use?",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi id autem quod quidem labore, voluptas fugit soluta, nostrum repellat mollitia dolorem ea velit nulla molestiae, nesciunt possimus. Inventore sapiente voluptatibus itaque neque vel! Doloremque, laboriosam.",
+  },
+  {
+    id: 5,
+    title: "When do I receive a password ordered by letter?",
+    info:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem velit quos atque labore amet debitis impedit veritatis magni exercitationem porro deserunt libero, nemo repellendus molestias voluptas cum maiores quisquam? At perferendis aliquid asperiores cumque eius amet vel culpa vitae similique, ipsam temporibus quisquam sit voluptas distinctio, nemo excepturi impedit tempore.",
+  },
+];
+export default questions;
