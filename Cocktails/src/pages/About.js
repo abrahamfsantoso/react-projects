1b2804d00d732dd75ffb156bb83fa3b2c57d8d81
Abraham Fajar Santoso <abrahamfsantoso@gmail.com>
2021-05-20 17:36:48 +0700

    added Cocktails

---
+++ Cocktails/src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sequi magni
+        eum nobis consequuntur animi earum harum, asperiores laboriosam rerum
+        aliquid aperiam inventore quasi vero. Eveniet quis tenetur, aliquam odit
+        velit alias sed porro sapiente itaque quasi. Blanditiis quibusdam
+        corporis animi, dolores, ipsa reprehenderit id eveniet in dignissimos
+        est quam adipisci.
+      </p>
+    </section>
+  );
+};
+
+export default About;
